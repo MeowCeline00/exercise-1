f92e7760df1c83398748b7c6b094d44190e804cf
MeowCeline00 <cwang325@my.bcit.ca>
2024-01-14 02:36:11 -0800

    Add query code

---
+++ components/Footer/index.js
@@ -0,0 +1,55 @@
+import styles from './Footer.module.css'
+
+export default function Footer() {
+    return (
+        <nav aria-label="Footer" >
+
+        <a href="https://about.twitter.com" style={{color: "rgb(113, 118, 123)"}}>
+        <span>About</span></a>
+        <span> </span>
+        <a href="https://help.twitter.com/using-x/download-the-x-app" style={{color: "rgb(113, 118, 123)"}}>
+        <span>Download the X app</span></a>
+        <span> </span>
+        <a href="https://help.twitter.com" style={{color: "rgb(113, 118, 123)"}}>
+        <span>Help Center</span></a>
+        <span> </span>
+        <a href="https://twitter.com/tos" style={{color: "rgb(113, 118, 123)"}}>
+        <span>Terms of Service</span></a>
+        <span> </span>
+        <a href="https://twitter.com/privacy" style={{color: "rgb(113, 118, 123)"}}>
+        <span>Privacy Policy</span></a>
+        <span> </span>
+        <a href="https://support.twitter.com/articles/20170514" style={{color: "rgb(113, 118, 123)"}}>
+        <span>Cookie Policy</span></a>
+        <span> </span>
+        <a href="https://help.twitter.com/resources/accessibility" style={{color: "rgb(113, 118, 123)"}}>
+        <span>Accessibility</span></a>
+        <span> </span>
+        <a href="https://business.twitter.com/en/help/troubleshooting/how-twitter-ads-work.html?ref=web-twc-ao-gbl-adsinfo&amp;utm_source=twc&amp;utm_medium=web&amp;utm_campaign=ao&amp;utm_content=adsinfo" style={{color: "rgb(113, 118, 123)"}}>
+        <span>Ads info</span></a>
+        <span> </span>
+        <a href="https://blog.twitter.com" style={{color: "rgb(113, 118, 123)"}}><span >Blog</span></a><a href="https://status.twitterstat.us" style={{color: "rgb(113, 118, 123)"}}>
+        <span>Status</span></a>
+        <span> </span>
+        <a href="https://careers.twitter.com" style={{color: "rgb(113, 118, 123)"}}><span >Careers</span></a><a href="https://about.twitter.com/press/brand-assets" style={{color: "rgb(113, 118, 123)"}}>
+        <span>Brand Resources</span></a>
+        <span> </span>
+        <a href="https://ads.twitter.com/?ref=gl-tw-tw-twitter-advertise" style={{color: "rgb(113, 118, 123)"}}><span >Advertising</span></a><a href="https://marketing.twitter.com" style={{color: "rgb(113, 118, 123)"}}>
+        <span>Marketing</span></a>
+        <span> </span>
+        <a href="https://business.twitter.com/?ref=web-twc-ao-gbl-twitterforbusiness&amp;utm_source=twc&amp;utm_medium=web&amp;utm_campaign=ao&amp;utm_content=twitterforbusiness" style={{color: "rgb(113, 118, 123)"}}>
+        <span>X for Business</span></a>
+        <span> </span>
+        <a href="https://developer.twitter.com" style={{color: "rgb(113, 118, 123)"}}>
+        <span>Developers</span></a>
+        <span> </span>
+        <a href="https://twitter.com/i/directory/profiles" style={{color: "rgb(113, 118, 123)"}}><span >Directory</span></a>
+        <span> </span>
+        <a href="/settings" style={{color: "rgb(113, 118, 123)"}}>
+        <span>Settings</span></a>
+        <span> </span>
+        <span style={{color: "rgb(113, 118, 123)"}}>© 2024 X Corp.</span>
+        
+        </nav>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
 import Orline from '@/components/Orline'
 import Button from '@/components/Button'
+import Footer from '@/components/Footer'
 
 export default function Home() {
   return (
@@ -45,60 +46,8 @@ export default function Home() {
           </div>
         </div>
         </div>
+        <Footer />
       </main>
-
-
-      <nav aria-label="Footer" >
-
-<a href="https://about.twitter.com" style={{color: "rgb(113, 118, 123)"}}>
-<span>About</span></a>
-<span> </span>
-<a href="https://help.twitter.com/using-x/download-the-x-app" style={{color: "rgb(113, 118, 123)"}}>
-<span>Download the X app</span></a>
-<span> </span>
-<a href="https://help.twitter.com" style={{color: "rgb(113, 118, 123)"}}>
-<span>Help Center</span></a>
-<span> </span>
-<a href="https://twitter.com/tos" style={{color: "rgb(113, 118, 123)"}}>
-<span>Terms of Service</span></a>
-<span> </span>
-<a href="https://twitter.com/privacy" style={{color: "rgb(113, 118, 123)"}}>
-<span>Privacy Policy</span></a>
-<span> </span>
-<a href="https://support.twitter.com/articles/20170514" style={{color: "rgb(113, 118, 123)"}}>
-<span>Cookie Policy</span></a>
-<span> </span>
-<a href="https://help.twitter.com/resources/accessibility" style={{color: "rgb(113, 118, 123)"}}>
-<span>Accessibility</span></a>
-<span> </span>
-<a href="https://business.twitter.com/en/help/troubleshooting/how-twitter-ads-work.html?ref=web-twc-ao-gbl-adsinfo&amp;utm_source=twc&amp;utm_medium=web&amp;utm_campaign=ao&amp;utm_content=adsinfo" style={{color: "rgb(113, 118, 123)"}}>
-<span>Ads info</span></a>
-<span> </span>
-<a href="https://blog.twitter.com" style={{color: "rgb(113, 118, 123)"}}><span >Blog</span></a><a href="https://status.twitterstat.us" style={{color: "rgb(113, 118, 123)"}}>
-<span>Status</span></a>
-<span> </span>
-<a href="https://careers.twitter.com" style={{color: "rgb(113, 118, 123)"}}><span >Careers</span></a><a href="https://about.twitter.com/press/brand-assets" style={{color: "rgb(113, 118, 123)"}}>
-<span>Brand Resources</span></a>
-<span> </span>
-<a href="https://ads.twitter.com/?ref=gl-tw-tw-twitter-advertise" style={{color: "rgb(113, 118, 123)"}}><span >Advertising</span></a><a href="https://marketing.twitter.com" style={{color: "rgb(113, 118, 123)"}}>
-<span>Marketing</span></a>
-<span> </span>
-<a href="https://business.twitter.com/?ref=web-twc-ao-gbl-twitterforbusiness&amp;utm_source=twc&amp;utm_medium=web&amp;utm_campaign=ao&amp;utm_content=twitterforbusiness" style={{color: "rgb(113, 118, 123)"}}>
-<span>X for Business</span></a>
-<span> </span>
-<a href="https://developer.twitter.com" style={{color: "rgb(113, 118, 123)"}}>
-<span>Developers</span></a>
-<span> </span>
-<a href="https://twitter.com/i/directory/profiles" style={{color: "rgb(113, 118, 123)"}}><span >Directory</span></a>
-<span> </span>
-<a href="/settings" style={{color: "rgb(113, 118, 123)"}}>
-<span>Settings</span></a>
-<span> </span>
-<div style={{color: "rgb(113, 118, 123)"}}>
-<span>© 2024 X Corp.</span></div>
-
-</nav>
-
 
     </>
   )
